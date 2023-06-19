ed7b2f8235f5b32821d3d3f95db34e6d9ac7fda8
6yu7h <114726087+6yu7h@users.noreply.github.com>
2023-06-19 19:35:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-lipsX = 0;
-lipsY = 0;
+noseX = 0;
+noseY = 0;
 
 function preload() {
- lips = loadImage('https://i.postimg.cc/hvRRsFsx/lips.jpg');
+ lips = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
 }
 
 function setup() {
@@ -26,14 +26,17 @@ function gotPoses(results)
 	if(results.length > 0)
 		{
 			console.log(results);
-			console.log("lips x =  " + results[0].pose.lips.x);
-			console.log("lips y =  " + results[0].pose.lips.y);
+			noseX = results[0].pose.nose.x-20;
+			noseY = results[0].pose.nose.y+10;
+			console.log("nose x =  " + noseX);
+			console.log("nose y =  " + noseY);
 		}
-}
+		}
 
 function draw()
 {
 	image(video, 0, 0, 300, 300);
+	image(lips, noseX, noseY, 40, 40);
 }
 
 function take_snapshot() {
